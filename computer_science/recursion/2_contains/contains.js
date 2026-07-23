985a154bb80a0e6e42e9184765e8cb0fd1de6ba2
DevScissors <sean.scissors@gmail.com>
2026-07-22 23:40:54 -0700

    Recusively going through a nested object and passing all tests

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,42 @@
-const contains = function() {
-  
+const tree = {
+  data: {
+    duplicate: "e",
+    stuff: {
+      thing: {
+        banana: NaN,
+        moreStuff: {
+          something: "foo",
+          answer: "test",
+        },
+      },
+    },
+    info: {
+      duplicate: "e",
+      magicNumber: 44,
+      empty: null,
+    },
+  },
 };
-  
+
+const contains = function (tree, targetValue) {
+  for (const key in tree) {
+    const value = tree[key];
+    if (Object.is(value, targetValue)) {
+      return true;
+    }
+    if (typeof value === "object" && value !== null) {
+      const result = contains(value, targetValue);
+
+      if (result) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
+
+contains(tree, 44);
+
 // Do not edit below this line
 module.exports = contains;
