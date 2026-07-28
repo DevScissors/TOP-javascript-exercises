226d341210f956d54cd3c0f6796612b3fda65375
DevScissors <sean.scissors@gmail.com>
2026-07-28 08:14:55 -0700

    Many tests still failing, including nested arrays or objects, but some tests are indeed passing

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,20 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (value) {
+  let totalIntCount = 0;
+  if (typeof value === "object" && value !== null && !Array.isArray(value)) {
+    const objValuesArr = Object.values(value);
+    totalIntegers(objValuesArr);
+  } else {
+    for (const item of value) {
+      if (typeof item === "number") {
+        totalIntCount++;
+      }
+    }
+  }
+  return totalIntCount;
 };
-  
+
+totalIntegers({ a: 1, b: "2", c: 3 }); // 2
+totalIntegers([1, 2, "3", 4]); // 3
+
 // Do not edit below this line
 module.exports = totalIntegers;
