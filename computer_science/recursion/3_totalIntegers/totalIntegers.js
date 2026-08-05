d15a2edc712957faf87fa4e93c3ccdb97d830393
DevScissors <sean.scissors@gmail.com>
2026-08-04 17:00:11 -0700

    All tests passing for total integers recursively

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,22 +1,21 @@
 const totalIntegers = function (value) {
   let totalIntCount = 0;
+  if (typeof value !== "object") {
+    return undefined;
+  }
   if (typeof value === "object" && value !== null && !Array.isArray(value)) {
     value = Object.values(value);
   }
   for (const item of value) {
-    if (typeof item === "number") {
+    if (Number.isInteger(item)) {
       totalIntCount++;
     }
     if (typeof item === "object" && item !== null) {
-      totalIntegers(item);
+      totalIntCount += totalIntegers(item);
     }
   }
   return totalIntCount;
 };
-
-// totalIntegers({ a: 1, b: "2", c: 3 }); // 2
-// totalIntegers([1, 2, "3", 4]); // 3
-totalIntegers([[[[[[[[[[[[[[4]]]]]], 246]]]]]]]]); // 2
 
 // Do not edit below this line
 module.exports = totalIntegers;
