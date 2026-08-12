f79bebe8856b42ccf8d00fa7cba6ae85edfa5acb
DevScissors <sean.scissors@gmail.com>
2026-08-12 07:45:13 -0700

    Finished permutations recursively passing all tests

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -12,23 +12,17 @@ const permutations = function (arr) {
 
   let results = [];
 
-  for (let i = 0; i < arr.length - 1; i++) {
-    let splitArr = arr.slice(1);
-    if (splitArr.length >= 3) {
-      permutations(splitArr);
-    }
-    let resultArr = insertToAllPlaces(arr[0], splitArr);
-    splitArr.reverse();
-    newArr = insertToAllPlaces(arr[0], splitArr);
-    results.push(...resultArr, ...newArr);
+  let splitArr = arr.slice(1);
+  let perms = permutations(splitArr);
+
+  for (let perm of perms) {
+    results.push(...insertToAllPlaces(arr[0], perm));
   }
 
   return results;
 };
 
 function insertToAllPlaces(element, arr) {
-  //element is 1, arr is [2, 3]
-
   let insertResults = [];
   for (let i = arr.length; i >= 0; i--) {
     let tempArr = [...arr];
@@ -37,8 +31,6 @@ function insertToAllPlaces(element, arr) {
   }
   return insertResults;
 }
-
-permutations([1, 2, 3, 4]);
 
 // Do not edit below this line
 module.exports = permutations;
